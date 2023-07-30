039d6706c8433602cf77832ea19329d66430f357
kavindujayarathna <kcjkavindu@gmail.com>
2023-07-30 18:50:24 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = 3000; // You can change this to any desired port number
+
+// Set static folder for serving CSS and JS files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Endpoint to serve the index.html page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
+
---
+++ public/script.js
@@ -0,0 +1,53 @@
+function appendToResult(value) {
+  const result = document.getElementById('result');
+  result.value += value;
+}
+
+async function calculate() {
+  const result = document.getElementById('result');
+  const { value } = result;
+  
+  try {
+    const apiUrl = 'http://65.2.145.132:8080/calculate';  
+
+    const requestBody = {
+      value,
+    };    
+
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    // Extract the 'answer' parameter from the JSON response
+    const { answer } = data.data;
+
+    result.value = answer
+  } catch (error) {
+    result.value = 'Error';
+  }
+}
+
+function clearResult() {
+  const result = document.getElementById('result');
+  result.value = '';
+}
+
+function testCalculate(expression) {
+  try {
+    const answer = eval(expression);
+    return answer;
+  } catch (error) {
+    return 'Error';
+  }
+}
+module.exports = testCalculate; 
